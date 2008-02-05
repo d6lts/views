d3b06159198cc90bbfb16615fee565a4a22f7153
Earl Miles <merlin@logrus.com>
2008-02-05 01:50:13 +0000

    Checkpoint checking. UI is getting better, but it is also getting further away from what currently runs, so when this is done a bunch of refactoring is going to have to happen.

---
+++ theme/views-view-row-node.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+/**
+ * @file views-view-row-node.tpl.php
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php print $node; ?>
