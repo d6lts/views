001ff7f402637b2f5e7d708a365046bb21812b46
Earl Miles <merlin@logrus.com>
2008-02-25 20:26:34 +0000

    Templateized exposed filters; exposed filter memory; exposed filter smoothing. Mostly done with exposed filters now; leaving exposed sorts for later.

---
+++ theme/views-exposed-form.tpl.php
@@ -0,0 +1,37 @@
+<?php
+// $Id$
+/**
+ * @file views-exposed-form.tpl.php
+ *
+ * This template handles the layout of the views exposed filter form.
+ *
+ * Variables available:
+ * - $widgets: An array of exposed form widgets. Each widget contains:
+ * - $widget->label: The visible label to print. May be optional.
+ * - $widget->operator: The operator for the widget. May be optional.
+ * - $widget->widget: The widget itself.
+ * - $button: The submit button for the form.
+ */
+?>
+<div class="views-exposed-form">
+  <div class="views-exposed-widgets clear-block">
+    <?php foreach($widgets as $id => $widget): ?>
+      <div class="views-exposed-widget">
+        <?php if (!empty($widget->label)): ?>
+          <label>
+            <?php print $widget->label; ?>
+          </label>
+        <?php endif; ?>
+        <?php if (!empty($widget->operator)): ?>
+          <div class="views-operator">
+            <?php print $widget->operator; ?>
+          </div>
+        <?php endif; ?>
+        <div class="views-widget">
+          <?php print $widget->widget; ?>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+  <?php print $button ?>
+</div>
