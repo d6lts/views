c31bfa130cfdc8d7d451ba1712237be2295d08df
Earl Miles <merlin@logrus.com>
2008-03-29 00:58:35 +0000

    Forgot to cvs add these files.

---
+++ theme/views-more.tpl.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+/**
+ * @file views-more.tpl.php
+ * Theme the more link
+ *
+ * - $more_url: the url for the more link
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<a href="<?php print $more_url ?>">View more</a>
