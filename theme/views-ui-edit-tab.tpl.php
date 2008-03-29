b57c309a6ca4eb1b1719a2dc7930fde397ec4588
Earl Miles <merlin@logrus.com>
2008-03-29 20:15:48 +0000

    #240102: (Largely by neclimdul) -- Initial pass at profile field handlers. Still 3 @todo items remain but they are not hard.

--- theme/views-ui-edit-tab.tpl.php
+++ theme/views-ui-edit-tab.tpl.php
@@ -5,7 +5,7 @@
  * Template for the primary view editing window.
  */
 ?>
-<div class="views-display views-display-<?php print $display->id; if (!empty($display->deleted)) { print ' views-display-deleted'; }; ?>">
+<div class="clear-block views-display views-display-<?php print $display->id; if (!empty($display->deleted)) { print ' views-display-deleted'; }; ?>">
   <?php // top section ?>
   <div class="top">
     <div class="inside">
