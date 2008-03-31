6c548b41248b1ec8e5b59754cdd141ee1ebef1aa
Earl Miles <merlin@logrus.com>
2008-03-31 17:56:15 +0000

    #240634: Due to use of field aliases that are not always valid, multiple pseudo fields weren't showing up. Fixed up the field template to better conform to other templates.

--- theme/views-view-fields.tpl.php
+++ theme/views-view-fields.tpl.php
@@ -4,12 +4,20 @@
  * @file views-view-fields.tpl.php
  * Default simple view template to all the fields as a row.
  *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
  * @ingroup views_templates
  */
 ?>
-<?php foreach ($fields as $name => $class): ?>
-  <div class="<?php print $class ?>">
-    <?php // this is indirection; $name contains the name of the variable. ?>
-    <?php print $$name; ?>
+<?php foreach ($fields as $id => $field): ?>
+  <div class="views-field-<?php print $field->class ?>">
+    <?php print $field->content; ?>
   </div>
 <?php endforeach; ?>
