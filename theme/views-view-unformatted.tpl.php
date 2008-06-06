bd2d1a12ca6eb90c2ae4605bfa99c2651b7fecc3
Earl Miles <merlin@logrus.com>
2008-06-06 22:43:08 +0000

    Introduce new "unformatted" summary style which can do inline.

--- theme/views-view-unformatted.tpl.php
+++ theme/views-view-unformatted.tpl.php
@@ -7,7 +7,7 @@
  * @ingroup views_templates
  */
 ?>
-<?php if (!empty($title)) : ?>
+<?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $row): ?>
