25c44cc190f96957ff153c35e1c37907d31e77b7
Earl Miles <merlin@logrus.com>
2008-09-24 22:48:21 +0000

    #298416: Add a mechanism of determining if a field needs SPAN or DIV tags.

--- theme/views-view-fields.tpl.php
+++ theme/views-view-fields.tpl.php
@@ -30,6 +30,10 @@
         <?php print $field->label; ?>:
       </label>
     <?php endif; ?>
-      <span class="field-content"><?php print $field->content; ?></span>
+      <?php
+      // $field->element_type is either SPAN or DIV depending upon whether or not
+      // the field is a 'block' element type or 'inline' element type.
+      ?>
+      <<?php print $field->element_type; ?> class="field-content"><?php print $field->content; ?></<?php print $field->element_type; ?>>
   </<?php print $field->inline_html;?>>
 <?php endforeach; ?>
