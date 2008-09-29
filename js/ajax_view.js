84809badf76b2cc834dfc0b151fc5bb908d1582d
Earl Miles <merlin@logrus.com>
2008-09-29 23:56:29 +0000

    Since it's not easily possible to do the views filter block externally, bring the functionality directly into Views, including modifying the ajax for that to work.

--- js/ajax_view.js
+++ js/ajax_view.js
@@ -47,51 +47,56 @@ Drupal.behaviors.ViewsAjaxView = function() {
   }
   if (Drupal.settings && Drupal.settings.views && Drupal.settings.views.ajaxViews) {
     $.each(Drupal.settings.views.ajaxViews, function(i, settings) {
-      var $view = $('.view-dom-id-' + settings.view_dom_id);
-      if (!$view.size()) {
+      var view = '.view-dom-id-' + settings.view_dom_id;
+      if (!$(view).size()) {
         // Backward compatibility: if 'views-view.tpl.php' is old and doesn't
         // contain the 'view-dom-id-#' class, we fall back to the old way of
         // locating the view:
-        $view = $('.view-id-' + settings.view_name + '.view-display-id-' + settings.view_display_id);
+        view = '.view-id-' + settings.view_name + '.view-display-id-' + settings.view_display_id;
       }
-      $view.filter(':not(.views-processed)').each(function() {
+
+      // Process exposed filter forms.
+      $('form#views-exposed-form-' + settings.view_name + '-' + settings.view_display_id)
+      .filter(':not(.views-processed)')
+      .each(function () {
+        // remove 'q' from the form; it's there for clean URLs
+        // so that it submits to the right place with regular submit
+        // but this method is submitting elsewhere.
+        $('input[name=q]', this).remove();
+        var form = this;
+        // ajaxSubmit doesn't accept a data argument, so we have to
+        // pass additional fields this way.
+        $.each(settings, function(key, setting) {
+          $(form).append('<input type="hidden" name="'+ key + '" value="'+ setting +'"/>');
+        });
+      })
+      .addClass('views-processed')
+      .submit(function () {
+        $('input[@type=submit]', this).after('<span class="views-throbbing">&nbsp</span>');
+        var object = this;
+        $(this).ajaxSubmit({
+          url: ajax_path,
+          type: 'GET',
+          success: function(response) {
+            // Call all callbacks.
+            if (response.__callbacks) {
+              $.each(response.__callbacks, function(i, callback) {
+                eval(callback)(view, response);
+              });
+              $('.views-throbbing', object).remove();
+            }
+          },
+          error: function() { alert(Drupal.t("An error occurred at ") + ajax_path); $('.views-throbbing', object).remove(); },
+          dataType: 'json'
+        });
+
+        return false;
+      });
+
+      $(view).filter(':not(.views-processed)').each(function() {
         var target = this;
         $(this)
         .addClass('views-processed')
-        // Process exposed filter forms.
-        .find('form#views-exposed-form')
-        .each(function () {
-          // remove 'q' from the form; it's there for clean URLs
-          // so that it submits to the right place with regular submit
-          // but this method is submitting elsewhere.
-          $('input[name=q]', this).remove();
-          var form = this;
-          // ajaxSubmit doesn't accept a data argument, so we have to
-          // pass additional fields this way.
-          $.each(settings, function(key, setting) {
-            $(form).append('<input type="hidden" name="'+ key + '" value="'+ setting +'"/>');
-          });
-        })
-        .submit(function () {
-          $('input[@type=submit]', this).after('<span class="views-throbbing">&nbsp</span>');
-          $(this).ajaxSubmit({
-            url: ajax_path,
-            type: 'GET',
-            success: function(response) {
-              // Call all callbacks.
-              if (response.__callbacks) {
-                $.each(response.__callbacks, function(i, callback) {
-                  eval(callback)(target, response);
-                });
-              }
-            },
-            error: function() { alert(Drupal.t("An error occurred at ") + ajax_path); },
-            dataType: 'json'
-          });
-  
-          return false;
-        })
-        .end()
         // Process pager links.
         .find('ul.pager > li > a')
         .each(function () {
