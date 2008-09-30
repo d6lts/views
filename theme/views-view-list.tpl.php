62151fc923a1dbed2cb97e599e0fb519c12edeeb
Earl Miles <merlin@logrus.com>
2008-09-30 19:47:11 +0000

    #281555: Add row classes (first, last, even, odd, row number) to the unformatted and list styles.

--- theme/views-view-list.tpl.php
+++ theme/views-view-list.tpl.php
@@ -14,8 +14,8 @@
     <h3><?php print $title; ?></h3>
   <?php endif; ?>
   <<?php print $options['type']; ?>>
-    <?php foreach ($rows as $row): ?>
-      <li><?php print $row ?></li>
+    <?php foreach ($rows as $id => $row): ?>
+      <li class="<?php print $classes[$id]; ?>"><?php print $row; ?></li>
     <?php endforeach; ?>
   </<?php print $options['type']; ?>>
 </div>
--- theme/views-view-unformatted.tpl.php
+++ theme/views-view-unformatted.tpl.php
@@ -10,6 +10,8 @@
 <?php if (!empty($title)): ?>
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
-<?php foreach ($rows as $row): ?>
-  <?php print $row ?>
+<?php foreach ($rows as $id => $row): ?>
+  <div class="<<?php print $classes[$id]; ?>">
+    <?php print $row; ?>
+  </div>
 <?php endforeach; ?>
