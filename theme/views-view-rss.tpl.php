93d0bc673c11a0864bf92bfbc15f28e64afba1bc
Earl Miles <merlin@logrus.com>
2008-12-02 00:02:06 +0000

    #338632 by drewish: Remove use of format_rss_channel and use proper templating instead.

--- theme/views-view-rss.tpl.php
+++ theme/views-view-rss.tpl.php
@@ -9,5 +9,12 @@
 ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
 <rss version="2.0" xml:base="<?php print $link; ?>"<?php print $namespaces; ?>>
-  <?php print $channel; ?>
+  <channel>
+    <title><?php print $title; ?></title>
+    <link><?php print $link; ?></link>
+    <description><?php print $description; ?></description>
+    <language><?php print $langcode; ?></language>
+    <?php print $channel_elements; ?>
+    <?php print $items; ?>
+  </channel>
 </rss>
