79ba0a1f470a9c675337ee03a48348147ccc2bbd
Earl Miles <merlin@logrus.com>
2009-01-27 22:11:26 +0000

    #353515: Many characters could get incorrectly translated in exposed filters combined with ajax tablesorting.

--- js/base.js
+++ js/base.js
@@ -69,7 +69,7 @@ Drupal.Views.parseQueryString = function (query) {
     var pair = pairs[i].split('=');
     // Ignore the 'q' path argument, if present.
     if (pair[0] != 'q' && pair[1]) {
-      args[pair[0]] = unescape(pair[1].replace(/\+/g, ' '));
+      args[pair[0]] = decodeURIComponent(pair[1].replace(/\+/g, ' '));
     }
   }
   return args;
