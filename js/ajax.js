26597e08986466666097829f5a919bdcc8a0f362
Earl Miles <merlin@logrus.com>
2009-11-18 20:17:07 +0000

    #366250 by Markus Petrux: Fix glitches with tabledrag and jQuery 1.3.

--- js/ajax.js
+++ js/ajax.js
@@ -309,5 +309,5 @@ Drupal.behaviors.ViewsAjaxLinks = function() {
  * Get rid of irritating tabledrag messages
  */
 Drupal.theme.tableDragChangedWarning = function () { 
-  return ' '; 
+  return '<div></div>';
 }
